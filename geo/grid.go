97ebaa14dbd7b8bc9e71e2786bab6a0258559eab
Tyler Trawick <veritablettrawick@gmail.com>
2018-10-30 00:39:32 -0400

    add geo distance function

--- geo/grid.go
+++ geo/grid.go
@@ -1,13 +1,10 @@
 package geo
 
-// given a lat-long rectangle, create a grid where each box is nxn meters
-// if the rectangle spans a long north-south distance there may be more boxes
-// at the top than at the bottom
+import (
+	"math"
+)
 
-// the grid is a slice where each element is a slice of boxes
-// each element in the primary array represents a swath latitude with height boxHeight
-
-const EarthRadiusInMeters float64 = 6371.009 * 1000
+const EarthRadiusInMeters float64 = 6371.0088 * 1000
 
 // a coordinate on the earth represented by lat / lon degrees
 type GeoPoint struct {
@@ -20,10 +17,34 @@ type GeoRect struct {
 	MinLat, MaxLat, MinLon, MaxLon float64
 }
 
+type cellCoord string
+
+// a geo grid is some GeoRect split into cells
+// of width CellWdith and height CellHeight.
+// The grid can contain points, which are represented as a map
+// from grid coordinates to a slice of GeoPoints
+// CellWidth and CellHeight are in meters
 type GeoGrid struct {
 	Boundary GeoRect
-	SubHeight, SubWidth int
-	// map from grid coordinate to point list
+	CellWidth, CellHeight int
+	Cells map[cellCoord][]GeoPoint
+}
+
+// assuming these points are only a few miles away (up to 30)
+func GeoDistance(p1, p2 GeoPoint) float64 {
+
+	dLat := p1.LatitudeInDegrees - p2.LatitudeInDegrees
+	dLat = math.Pi * dLat / 180
+
+	dLon := p1.LongitudeInDegrees - p2.LongitudeInDegrees
+	dLon = math.Pi * dLon / 180
+
+	// take the average latitude and use that to shorten the latitude circle
+	avgLat := (p1.LatitudeInDegrees + p2.LatitudeInDegrees) / 2
+	avgLat = math.Pi * avgLat / 180
+	dLon = math.Cos(avgLat) * dLon
+	
+	return EarthRadiusInMeters * math.Sqrt((dLat * dLat) + (dLon * dLon))
 }
 
 func GridBoundary(paths ...[]GeoPoint) GeoRect {
@@ -53,3 +74,41 @@ func GridBoundary(paths ...[]GeoPoint) GeoRect {
 	return r
 }
 
+// draws a GeoGrid onto a set of paths
+func PathToGeoGrid(cellWidth, cellHeight int, paths ...[]GeoPoint) *GeoGrid {
+	
+	boundary := GridBoundary(paths...)
+
+	var cells map[cellCoord][]GeoPoint
+
+	grid := GeoGrid{
+		Boundary: boundary,
+		CellWidth: cellWidth,
+		CellHeight: cellHeight,
+		Cells: cells,
+	}
+
+	for _, path := range paths {
+		for _, point := range path {
+			coord := cellForPoint(point, &grid)
+			if grid.Cells[coord] == nil {
+				grid.Cells[coord] = make([]GeoPoint, 1)
+			}
+			grid.Cells[coord] = append(grid.Cells[coord], point)
+		}
+	}
+	return &grid
+}
+
+func cellForPoint(point GeoPoint, grid *GeoGrid) cellCoord {
+	// do lat and long separately
+	// latDistance := grid.Boundary.MaxLat - grid.Boundary.MinLat
+
+	// what's the difference in meters between latDistance at minLon and latDistance at maxLon
+	// over how many rows would you need to add another cell?
+	// the grid has a ragged edge
+
+	// distanceInMeters := GeoDistance(GeoPoint{ }, GeoPoint{ })
+
+	return cellCoord("0_0")
+}
--- geo/grid_test.go
+++ geo/grid_test.go
@@ -3,6 +3,7 @@ package geo
 import(
 	"testing"
 	"reflect"
+	"math"
 )
 
 func TestGridBoundary(t *testing.T) {
@@ -67,4 +68,43 @@ func TestGridBoundary(t *testing.T) {
 	if !reflect.DeepEqual(boundary2, t2) {
 		t.Errorf("incorrect boundary structure for 3-path example")
 	}
+}
+
+func TestGeoDistance(t *testing.T) {
+
+	// two points relatively close together (half a mile)
+	d1 := GeoDistance(GeoPoint{42.353381, -71.107131}, GeoPoint{42.356941, -71.092647})
+	if math.Abs(d1 - 1250) > 7 {
+		t.Errorf("GeoDistance off for two close points")
+	}
+
+	// two points very close together (a few feet)
+	d2 := GeoDistance(GeoPoint{42.364378, -71.114549}, GeoPoint{42.364447, -71.114603})
+	if math.Abs(d2 - 10) > 7 {
+		t.Errorf("GeoDistance off for two very close points")
+	}
+	
+	// the same two points
+	d3 := GeoDistance(GeoPoint{42.365673, -71.104100}, GeoPoint{42.365673, -71.104100})
+	if d3 != 0 {
+		t.Errorf("GeoDistance off for exact two points")
+	}
+	
+	// two points at the same longitude
+	d4 := GeoDistance(GeoPoint{42.365673, -71.104100}, GeoPoint{42.845683, -71.104100})
+	if math.Abs(d4 - 53370) > 7 {
+		t.Errorf("GeoDistance off for two points on same line of longitude")
+	}
+	
+	// two points at the same latitude
+	d5 := GeoDistance(GeoPoint{42.365673, -71.304331}, GeoPoint{42.365673, -71.104100})
+	if math.Abs(d5 - 16450) > 7 {
+		t.Errorf("GeoDistance off for two points on same line of latitude")
+	}
+	
+	// two points relatively far away with varying lat / lon (~10 kilometers)
+	d6 := GeoDistance(GeoPoint{42.365121, -71.212806}, GeoPoint{42.366810, -71.068591})
+	if math.Abs(d6 - 11850) > 7 {
+		t.Errorf("GeoDistance off for two relatively distant points")
+	}	
 }
