425576cd99329ebcbbe450e9e066036776143873
Tyler Trawick <veritablettrawick@gmail.com>
2018-11-08 21:54:03 -0500

    geo grid function

--- geo/grid.go
+++ geo/grid.go
@@ -2,9 +2,12 @@ package geo
 
 import (
 	"math"
+	"fmt"
 )
 
 const EarthRadiusInMeters float64 = 6371.0088 * 1000
+// a change in one degree of latitude along a meridian is about 111.32km
+const LatitudeDegreesToMeters float64 = 111.32 * 1000
 
 // a coordinate on the earth represented by lat / lon degrees
 type GeoPoint struct {
@@ -14,7 +17,7 @@ type GeoPoint struct {
 // the minimum area bounded by the four lat / lon lines
 // all values in degrees
 type GeoRect struct {
-	MinLat, MaxLat, MinLon, MaxLon float64
+	NorthWest, SouthEast GeoPoint
 }
 
 type cellCoord string
@@ -26,11 +29,12 @@ type cellCoord string
 // CellWidth and CellHeight are in meters
 type GeoGrid struct {
 	Boundary GeoRect
-	CellWidth, CellHeight int
+	CellWidth, CellHeight float64
 	Cells map[cellCoord][]GeoPoint
 }
 
 // assuming these points are only a few miles away (up to 30)
+// always returns a positive real number
 func GeoDistance(p1, p2 GeoPoint) float64 {
 
 	dLat := p1.LatitudeInDegrees - p2.LatitudeInDegrees
@@ -49,37 +53,39 @@ func GeoDistance(p1, p2 GeoPoint) float64 {
 
 func GridBoundary(paths ...[]GeoPoint) GeoRect {
 
-	r := GeoRect{
-		MinLat: 91,
-		MaxLat: -91,
-		MinLon: 181,
-		MaxLon: -181,
-	}
+	minLat := float64(91)
+	maxLat := float64(-91)
+	minLon := float64(181)
+	maxLon := float64(-181)
+
 	for _, path := range(paths) {
 		for _, pt := range(path) {
-			if pt.LatitudeInDegrees < r.MinLat {
-				r.MinLat = pt.LatitudeInDegrees
+			if pt.LatitudeInDegrees < minLat {
+				minLat = pt.LatitudeInDegrees
 			}
-			if pt.LatitudeInDegrees > r.MaxLat {
-				r.MaxLat = pt.LatitudeInDegrees
+			if pt.LatitudeInDegrees > maxLat {
+				maxLat = pt.LatitudeInDegrees
 			}
-			if pt.LongitudeInDegrees < r.MinLon {
-				r.MinLon = pt.LongitudeInDegrees
+			if pt.LongitudeInDegrees < minLon {
+				minLon = pt.LongitudeInDegrees
 			}
-			if pt.LongitudeInDegrees > r.MaxLon {
-				r.MaxLon = pt.LongitudeInDegrees
+			if pt.LongitudeInDegrees > maxLon {
+				maxLon = pt.LongitudeInDegrees
 			}
 		}
 	}
-	return r
+	return GeoRect{
+		NorthWest: GeoPoint{maxLat, minLon},
+		SouthEast: GeoPoint{minLat, maxLon},
+	}
 }
 
-// draws a GeoGrid onto a set of paths
-func PathToGeoGrid(cellWidth, cellHeight int, paths ...[]GeoPoint) *GeoGrid {
+// map each point in a set of paths to a coordinate on a GeoGrid
+func PathToGeoGrid(cellWidth, cellHeight float64, paths ...[]GeoPoint) *GeoGrid {
 	
 	boundary := GridBoundary(paths...)
 
-	var cells map[cellCoord][]GeoPoint
+	cells := make(map[cellCoord][]GeoPoint)
 
 	grid := GeoGrid{
 		Boundary: boundary,
@@ -101,14 +107,53 @@ func PathToGeoGrid(cellWidth, cellHeight int, paths ...[]GeoPoint) *GeoGrid {
 }
 
 func cellForPoint(point GeoPoint, grid *GeoGrid) cellCoord {
-	// do lat and long separately
-	// latDistance := grid.Boundary.MaxLat - grid.Boundary.MinLat
 
-	// what's the difference in meters between latDistance at minLon and latDistance at maxLon
-	// over how many rows would you need to add another cell?
-	// the grid has a ragged edge
+	minLat := grid.Boundary.SouthEast.LatitudeInDegrees
+	maxLat := grid.Boundary.NorthWest.LatitudeInDegrees
+	minLon := grid.Boundary.NorthWest.LongitudeInDegrees
+	maxLon := grid.Boundary.SouthEast.LongitudeInDegrees
 
-	// distanceInMeters := GeoDistance(GeoPoint{ }, GeoPoint{ })
+	// meridian distances don't vary with latitude / longitude
+	meridianDistance := GeoDistance(GeoPoint{minLat, minLon},
+		GeoPoint{maxLat, minLon})
 
-	return cellCoord("0_0")
+	pointOffset := GeoDistance(GeoPoint{point.LatitudeInDegrees, minLon},
+		GeoPoint{maxLat, minLon})
+
+	latIndex := cellSearch(0, meridianDistance, grid.CellHeight, pointOffset, 0)
+
+	// however, distances along a parallel vary with latitude
+	parallelDistance := GeoDistance(GeoPoint{point.LatitudeInDegrees, minLon},
+		GeoPoint{point.LatitudeInDegrees, maxLon})
+
+	pointOffset = GeoDistance(GeoPoint{minLat, point.LongitudeInDegrees},
+		GeoPoint{minLat, maxLon})
+
+	lonIndex := cellSearch(0, parallelDistance, grid.CellWidth, pointOffset, 0)
+	
+	return cellCoord(fmt.Sprintf("%d_%d", latIndex, lonIndex))
+}
+
+// binary search through a given range to find the index of the cell
+func cellSearch(min, max, cellSize, loc float64, offset int) int {
+
+	if max - min <= cellSize {
+		return offset
+	}
+
+	// the "right-dividing box index" or the index to the cell
+	// to the right of the midpoint line of the current range
+	rdbi := int(math.Floor(math.Ceil((max - min) / cellSize) / 2))
+	midpoint := cellSize * float64(rdbi + offset)
+
+	if loc == midpoint {
+		return rdbi - 1
+	} else if loc > midpoint {
+		min = midpoint
+		offset += rdbi
+		return cellSearch(min, max, cellSize, loc, offset)
+	} else {
+		max = midpoint
+		return cellSearch(min, max, cellSize, loc, offset)
+	}
 }
--- geo/grid_test.go
+++ geo/grid_test.go
@@ -4,19 +4,39 @@ import(
 	"testing"
 	"reflect"
 	"math"
+	"strings"
+	"strconv"
+	"fmt"
 )
 
-func TestGridBoundary(t *testing.T) {
-	// first create some test cases
+var path1 = []GeoPoint{
+	GeoPoint{42.365592, -71.103875},
+	GeoPoint{42.364776, -71.110749},
+	GeoPoint{42.364237, -71.116022},
+	GeoPoint{42.361439, -71.115968},
+	GeoPoint{42.362285, -71.113515},
+	GeoPoint{42.365115, -71.104975},
+}
 
-	path1 := []GeoPoint{
-		GeoPoint{42.365592, -71.103875},
-		GeoPoint{42.364776, -71.110749},
-		GeoPoint{42.364237, -71.116022},
-		GeoPoint{42.361439, -71.115968},
-		GeoPoint{42.362285, -71.113515},
-		GeoPoint{42.365115, -71.104975},
-	}
+var path2 = []GeoPoint{
+	GeoPoint{42.365218, -71.104578},
+	GeoPoint{42.362285, -71.113429},
+	GeoPoint{42.360644, -71.113225},
+	GeoPoint{42.360192, -71.112774},
+	GeoPoint{42.364037, -71.108278},
+	GeoPoint{42.365223, -71.104852},
+}
+
+var path3 = []GeoPoint{
+	GeoPoint{42.365127, -71.103168},
+	GeoPoint{42.360831, -71.096162},
+	GeoPoint{42.359000, -71.100175},
+	GeoPoint{42.361164, -71.103930},
+	GeoPoint{42.365516, -71.103951},
+	GeoPoint{42.364667, -71.102567},
+}
+	
+func TestGridBoundary(t *testing.T) {
 
 	// find the min / max lat long for one path
 	boundary1 := GridBoundary(path1)
@@ -29,29 +49,14 @@ func TestGridBoundary(t *testing.T) {
 	//   minLon: -71.116022
 	// }
 
-	t1 := GeoRect{42.361439, 42.365592, -71.116022, -71.103875}
+	t1 := GeoRect{
+		NorthWest: GeoPoint{42.365592, -71.116022},
+		SouthEast: GeoPoint{42.361439, -71.103875},
+	}
 	if !reflect.DeepEqual(boundary1, t1) {
 		t.Errorf("incorrect boundary structure for 1-path example")
 	}
 
-	path2 := []GeoPoint{
-		GeoPoint{42.365218, -71.104578},
-		GeoPoint{42.362285, -71.113429},
-		GeoPoint{42.360644, -71.113225},
-		GeoPoint{42.360192, -71.112774},
-		GeoPoint{42.364037, -71.108278},
-		GeoPoint{42.365223, -71.104852},
-	}
-
-	path3 := []GeoPoint{
-		GeoPoint{42.365127, -71.103168},
-		GeoPoint{42.360831, -71.096162},
-		GeoPoint{42.359000, -71.100175},
-		GeoPoint{42.361164, -71.103930},
-		GeoPoint{42.365516, -71.103951},
-		GeoPoint{42.364667, -71.102567},
-	}
-	
 	// then for multiple
 	boundary2 := GridBoundary(path1, path2, path3)
 
@@ -63,7 +68,10 @@ func TestGridBoundary(t *testing.T) {
 	//   minLon: -71.116022
 	// }
 
-	t2 := GeoRect{42.359000, 42.365592, -71.116022, -71.096162}
+	t2 := GeoRect{
+		NorthWest: GeoPoint{42.365592, -71.116022},
+		SouthEast: GeoPoint{42.359000, -71.096162},
+	}
 
 	if !reflect.DeepEqual(boundary2, t2) {
 		t.Errorf("incorrect boundary structure for 3-path example")
@@ -106,5 +114,84 @@ func TestGeoDistance(t *testing.T) {
 	d6 := GeoDistance(GeoPoint{42.365121, -71.212806}, GeoPoint{42.366810, -71.068591})
 	if math.Abs(d6 - 11850) > 7 {
 		t.Errorf("GeoDistance off for two relatively distant points")
+	}	
+}
+
+
+func TestPathToGeoGrid(t *testing.T) {
+
+	width := 7.0
+	height := 7.0
+
+	g1 := PathToGeoGrid(width, height, path1)
+
+	// some sanity checks on the grid
+	if (g1.CellWidth != width) {
+		t.Errorf("grid cell width differs from input")
+	}
+	if (g1.CellHeight != height) {
+		t.Errorf("grid cell height differs from input")
+	}
+	b1 := GeoRect{
+		NorthWest: GeoPoint{42.365592, -71.116022},
+		SouthEast: GeoPoint{42.361439, -71.103875},
+	}
+	if !reflect.DeepEqual(g1.Boundary, b1) {
+		t.Errorf("grid boundary does not match expected")
+	}
+	// checking the cells is more involved
+	// split it off into its own function
+	checkCells(t, g1)
+}
+
+func checkCells(t *testing.T, grid *GeoGrid) {
+
+	minLatCoord := math.MaxInt64
+	maxLatCoord := -1
+	minLonCoord := math.MaxInt64
+	maxLonCoord := -1
+	for coord, _ := range(grid.Cells) {
+		parts := strings.Split(string(coord), "_")
+
+		latCoord, _ := strconv.Atoi(parts[0])
+		if latCoord > maxLatCoord {
+			maxLatCoord = latCoord
+		} else if latCoord < minLatCoord {
+			minLatCoord = latCoord
+		}
+
+		lonCoord, _ := strconv.Atoi(parts[1])
+		if lonCoord > maxLonCoord {
+			
+			maxLonCoord = lonCoord
+		} else if lonCoord < minLonCoord {
+			minLonCoord = lonCoord
+		}
+	}
+
+	minLat := grid.Boundary.SouthEast.LatitudeInDegrees
+	maxLat := grid.Boundary.NorthWest.LatitudeInDegrees
+	minLon := grid.Boundary.NorthWest.LongitudeInDegrees
+	maxLon := grid.Boundary.SouthEast.LongitudeInDegrees
+
+	meridianDistance := GeoDistance(GeoPoint{minLat, minLon}, GeoPoint{maxLat, minLon})
+	parallelDistance := GeoDistance(GeoPoint{minLat, minLon}, GeoPoint{minLat, maxLon})
+
+	// remember, distance along a parallel is a change in longitude
+	maxLonCell := int(math.Floor(parallelDistance / grid.CellWidth))
+	// and distance along a meridian is a change in latitude
+	maxLatCell := int(math.Floor(meridianDistance / grid.CellHeight))
+
+	if minLatCoord < 0 {
+		t.Errorf("minimum latitude grid coordinate should be non-negative")
+	}
+	if minLonCoord < 0 {
+		t.Errorf("minimum longitude grid coordinate should be non-negative")
+	}
+	if maxLatCoord > maxLatCell {
+		t.Errorf(fmt.Sprintf("maximum latitude grid coordinate should less than %d", maxLatCell))
+	}
+	if maxLonCoord > maxLonCell {
+		t.Errorf(fmt.Sprintf("maximum longitude grid coordinate should less than %d", maxLonCell))
 	}	
 }
